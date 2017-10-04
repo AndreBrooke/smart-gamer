a9b6984722fe6aedb6eb0cb7fd8a7fb613ca1824
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-10-04 12:42:29 +0800

    omniauth steam token

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+	root 'clearance/sessions#new'
 	match '/auth/:provider/callback', to: 'sessions#create', via: :all
 	delete '/logout', to: 'sessions#destroy', as: :logout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20171003103742) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", null: false
+    t.string "email"
     t.string "encrypted_password", limit: 128
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
