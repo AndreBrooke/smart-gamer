45fc227c96045d35579ef19d1889485145105918
GeorgeSalari <x3m.giku@gmail.com>
2017-10-04 18:21:36 +0800

    fix helpers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,19 @@ class SessionsController < ApplicationController
     if !@user.save
       flash[:error] = "Login Fail"
     elsif @user.email == "example@na.com"
+      sign_in(@user)
     	flash[:notice] = "Add your email please"
     	redirect_to edit_user_path(@user.id)
     else
-      session[:user_id] = @user.id
+      sign_in(@user)
       flash[:success] = "Welcome, #{@user.nickname}!"
       redirect_to user_path(@user)
     end
   end
+
   def destroy
 	  if current_user
-	    session.delete(:user_id)
+	    sign_out
 	    flash[:success] = "Goodbye!"
 	  end
 	  	redirect_to root_path
