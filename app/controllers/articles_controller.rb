ad6ac0c827014dd154b66b1c157f203c7722b1fc
leesc22 <leesiewching22@gmail.com>
2017-10-04 17:42:22 +0800

    articles controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:success] = "Article is created successfully."
+			redirect_to @article
+		else
+			flash[:danger] = "Error creating article."
+			render 'new'
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:success] = "Article is updated successfully."
+			redirect_to @article
+		else
+			flash[:danger] = "Error updating article."
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_back(fallback_location: root_path)
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :content)
+	end
+end
--- db/migrate/20171003103742_create_articles.rb
+++ db/migrate/20171003103742_create_articles.rb
@@ -1,7 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
-
+    	t.string :url
+    	t.string :title
+    	t.text :content
+    	t.string :image
       t.timestamps
     end
   end
