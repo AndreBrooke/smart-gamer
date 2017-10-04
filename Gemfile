682127912c08e55d7476298239645a454e7b5d9b
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-10-04 12:18:42 +0800

    user table generated

--- Gemfile
+++ Gemfile
@@ -30,8 +30,10 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem "clearance"
-
+# Use figaro for privacy protection
 gem 'figaro'
+# Use Omniauth Steam for steam login
+gem 'omniauth-steam'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/migrate/20171003103714_create_users.rb
+++ db/migrate/20171003103714_create_users.rb
@@ -3,12 +3,19 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.timestamps null: false
       t.string :email, null: false
-      t.string :encrypted_password, limit: 128, null: false
+      t.string :encrypted_password, limit: 128, null: true
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
+      t.string :uid
+      t.string :nickname
+      t.string :avatar_url
+      t.string :profile_url
+      t.string :name
     end
 
     add_index :users, :email
     add_index :users, :remember_token
+    add_index :users, :uid, unique: true
+
   end
 end
