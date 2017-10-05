f81e2dfa6785244b1ff8c4ae89e20ac6390d8449
hjchan <haujiechan@gmail.com>
2017-10-05 10:25:02 +0800

    small change in playtime model file

--- app/models/playtime.rb
+++ app/models/playtime.rb
@@ -6,11 +6,10 @@ class Playtime < ApplicationRecord
 
   def update_today_playtime
     yesterday = user.playtimes.find_by(date: Date.today-1)
-    # byebug
     if yesterday
       self.today_playtime = self[:total_playtime] - yesterday[:total_playtime]
     else
-      self.today_playtime = 20
+      self.today_playtime = nil
     end
   end
 
