31d6b0d53a73b974cdf0fe89d8040e15225f80fe
leesc22 <leesiewching22@gmail.com>
2017-10-05 10:54:51 +0800

    Style article item with border, margin and padding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# rake db:seed
+
 include ArticlesHelper
 
 url = "https://www.flaktest.com/studied-benefits-playing-esports/"
