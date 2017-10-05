8b814e214582d29bdcacaf8f68a047beb21aa152
leesc22 <leesiewching22@gmail.com>
2017-10-05 11:36:57 +0800

    insert back font awesome and bootstrap social cdn

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20171005024558) do
   end
 
   create_table "articles", force: :cascade do |t|
+    t.string "url"
+    t.string "title"
+    t.text "content"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
