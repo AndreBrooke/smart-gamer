191abbc9ca95d3d862521678ebcfb25fd73584cf
leesc22 <leesiewching22@gmail.com>
2017-10-05 10:28:22 +0800

    Add seed for articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+include ArticlesHelper
+
+url = "https://www.flaktest.com/studied-benefits-playing-esports/"
+Article.create(get_shared_article_params(url))
+
+url = "http://es.me/en/2017/08/18/another-esports-academy-opens-doors-malaysia/"
+Article.create(get_shared_article_params(url))
+
+Article.create(title: "Stay tuned for our upcoming event", content: "Hint: new badge")
