117dabc481587452defbf8a2cea79a7886209ef0
leesc22 <leesiewching22@gmail.com>
2017-10-06 17:38:23 +0800

    reply article will update achievement progress

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
 	belongs_to :user
   belongs_to :article
+  before_save :update_achievement_progress
+
+  def update_achievement_progress
+    badges = Badge.where("name ILIKE ?", "%reply%")
+    badges.each do |badge|
+      achievement = self.user.achievements.find_by(badge_id: badge.id)
+      achievement.increment!(:progress)
+    end
+  end
 end
