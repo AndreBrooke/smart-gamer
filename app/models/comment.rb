dc420049d35d66fa53dc9fb212e45292f46c624e
leesc22 <leesiewching22@gmail.com>
2017-10-06 22:30:58 +0800

    create activity when user leave a comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
 	belongs_to :user
   belongs_to :article
-  before_save :update_achievement_progress
+  before_save :update_achievement_progress, :create_activity
 
   def update_achievement_progress
     badges = Badge.where("name ILIKE ?", "%reply%")
@@ -12,6 +12,12 @@ class Comment < ApplicationRecord
         achievement.update_attribute(:status, true) 
         self.user.activities.create(content: "#{self.user.nickname} unlocked a new badge - #{badge.name}")
       end
+    end
+  end
+
+  def create_activity
+    unless self == Comment.last
+      self.user.activities.create(content: "#{self.user.nickname} posted a comment to #{self.article.title}")
     end
   end
 end
