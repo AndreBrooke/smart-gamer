96f0eb6e14c0f6d845d2bc93c87f7c264ce7fd2a
leesc22 <leesiewching22@gmail.com>
2017-10-09 12:52:47 +0800

    only admin create/update articles

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -21,4 +21,9 @@ module HomeHelper
 			[]
 		end
 	end
+
+	def get_owned_games(user)
+    steam_id = user.uid.to_i + 76561197960265728
+    RestClient.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=1DB6B5F9C6DA54FE150CFCABCB94447A&steamid=#{steam_id}&include_appinfo=1&format=json")
+	end
 end
