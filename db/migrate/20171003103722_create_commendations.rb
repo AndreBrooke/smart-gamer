07559d976b8a94d734ccb19443b88af163ca599e
GeorgeSalari <x3m.giku@gmail.com>
2017-10-09 19:25:07 +0800

    add commendation to new user

--- db/migrate/20171003103722_create_commendations.rb
+++ db/migrate/20171003103722_create_commendations.rb
@@ -1,7 +1,6 @@
 class CreateCommendations < ActiveRecord::Migration[5.1]
   def change
     create_table :commendations do |t|
-    	t.string :image
     	t.string :name
     	t.integer :count, default: 0
     	t.belongs_to :user
