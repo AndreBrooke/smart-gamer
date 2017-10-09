3bc3fce8240c71a876eaed95a9493cba5daa5cc4
leesc22 <leesiewching22@gmail.com>
2017-10-09 19:40:34 +0800

    fix follow button

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,16 +1,16 @@
 class FollowersController < ApplicationController
   def create
-    @user = User.find(params[:follower][:user_id])
+    @user = User.find(params[:follower][:follower_id])
     followers = Follower.where(status: true)
     @follower = followers.find_or_initialize_by(follower_params)
     if @follower.id
-      @follower.toggle!(:status)
+      @follower.update(status: !status)
       flash[:notice] = "You unfollow #{@user.nickname}"
-      redirect_to user_path(@follower)
+      redirect_to user_path(@follower.follower_id)
     else
       if @follower.save
         flash[:notice] = "You follow #{@user.nickname}"
-        redirect_to user_path(@follower)
+        redirect_to user_path(@follower.follower_id)
       else
         flash[:warning] = "#{@follower.errors.messages[0]}"
       end
