3e2707f7304efdc86a9c54480870b2f9bb0ca306
WilsonkwSheng <wilsonkhoo93@gmail.com>
2017-10-10 11:30:51 +0800

    add toxicidity feature in user stats

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,9 @@ module UsersHelper
 		steam_id = user.uid.to_i + 76561197960265728
 		RestClient.get("http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=1DB6B5F9C6DA54FE150CFCABCB94447A&steamid=#{steam_id}&include_appinfo=1&include_played_free_games=1&format=json")
 	end
+
+	def get_toxicidity_score(user)
+		steam_id = user.uid.to_i
+		RestClient.get("https://api.opendota.com/api/players/#{steam_id}/wordcloud")
+	end
 end
